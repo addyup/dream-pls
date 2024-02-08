b55e0ae5b568fa7292aa175239b02e8305a5c459
adduup <adwait.uprety21@gmail.com>
2024-02-08 17:37:44 +0545

    1

---
+++ parseLoader/src/main/java/com/parseLoader/parseLoader/controller/Controller.java
@@ -0,0 +1,29 @@
+package com.parseLoader.parseLoader.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.parseLoader.parseLoader.entity.HomepageForm;
+
+@RestController
+public class Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+    @PostMapping(path= "/homepageFormSubmission", consumes = "application/json", produces = "application/json")
+    @CrossOrigin
+    public ResponseEntity<HomepageForm> getHomepageFormSubmission(@RequestBody HomepageForm homepageForm) {
+        logger.info("response for Homepageform ===> " + homepageForm.getBuilderType()
+        + " " + homepageForm.getInputEmail() + " " + homepageForm.getInputFullName()
+        + " " + homepageForm.getInputWebsite() + " " + homepageForm.getInputZip() 
+        + " " + homepageForm.getInputisBorC() + " " + homepageForm.getCountryType() 
+        + " " + homepageForm.getInputRefer());
+        return ResponseEntity.ok().build();
+
+    }
+}
---
+++ parseLoader/src/main/java/com/parseLoader/parseLoader/entity/HomepageForm.java
@@ -0,0 +1,95 @@
+package com.parseLoader.parseLoader.entity;
+
+
+public class HomepageForm {
+
+    
+  private String builderType;
+  private String countryType;
+  private String inputEmail;
+  private String inputWebsite;
+  private String inputFullName;
+  private String inputisBorC;
+  private String inputZip;
+  private boolean inputRefer;
+
+  // Getter
+  public String getBuilderType() {
+    return builderType;
+  }
+
+  // Setter
+  public void setBuilderType(String builderType) {
+    this.builderType = builderType;
+  }
+
+  // Getter
+  public String getCountryType() {
+    return countryType;
+  }
+
+  // Setter
+  public void setCountryType(String countryType) {
+    this.countryType = countryType;
+  }
+
+  // Getter
+  public String getInputEmail() {
+    return inputEmail;
+  }
+
+  // Setter
+  public void setInputEmail(String inputEmail) {
+    this.inputEmail = inputEmail;
+  }
+
+  // Getter
+  public String getInputWebsite() {
+    return inputWebsite;
+  }
+
+  // Setter
+  public void setInputWebsite(String inputWebsite) {
+    this.inputWebsite = inputWebsite;
+  }
+
+  // Getter
+  public String getInputFullName() {
+    return inputFullName;
+  }
+
+  // Setter
+  public void setInputFullName(String inputFullName) {
+    this.inputFullName = inputFullName;
+  }
+
+  // Getter
+  public String getInputisBorC() {
+    return inputisBorC;
+  }
+
+  // Setter
+  public void setInputisBorC(String inputisBorC) {
+    this.inputisBorC = inputisBorC;
+  }
+
+  // Getter
+  public String getInputZip() {
+    return inputZip;
+  }
+
+  // Setter
+  public void setInputZip(String inputZip) {
+    this.inputZip = inputZip;
+  }
+
+  // Getter
+  public boolean getInputRefer() {
+    return inputRefer;
+  }
+
+  // Setter
+  public void setInputRefer(boolean inputRefer) {
+    this.inputRefer = inputRefer;
+  }
+}
