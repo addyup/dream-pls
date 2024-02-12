44d99072d19f2867687a3b0a30dac551ea2e34a7
adduup <adwait.uprety21@gmail.com>
2024-02-13 05:26:23 +0545

    removed loader
    plan to migrate to node

--- dream-builders/js/homepageFormSubmission.js
+++ dream-builders/js/homepageFormSubmission.js
@@ -1,20 +1,6 @@
 function sumbitFormDetailsToPostAPI_HOMEPAGEDetails() {
-
   document.getElementById("homepageForm").submit(); 
 
-     $.post("http://localhost:8080/homepageFormSubmission",
-    {
-      builderType: document.getElementById("builderType").value, 
-      countryType: document.getElementById("countryType").value,
-      inputEmail: document.getElementById("inputEmail").value,
-      inputWebsite: document.getElementById("inputWebsite").value,
-      inputFulllName: document.getElementById("inputFullName").value,
-      inputisBorC: document.getElementById("inputisBorC").value,
-      inputZip: document.getElementById("inputZip").value,
-      inputRefer: document.getElementById("inputRefer").value,
-    },
-    function(data,status){
-      alert("Data: " + data + "\nStatus: " + status);
-    });
+  alert("Email sent to " + document.getElementById("inputEmail").value);
 }
 
