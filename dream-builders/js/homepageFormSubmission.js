3867959dd7f18e6cc69dafd4155e9d1322612364
adduup <adwait.uprety21@gmail.com>
2024-02-13 21:27:54 +0545

    push db file mt

--- dream-builders/js/homepageFormSubmission.js
+++
@@ -1,6 +0,0 @@
-function sumbitFormDetailsToPostAPI_HOMEPAGEDetails() {
-  document.getElementById("homepageForm").submit(); 
-
-  alert("Email sent to " + document.getElementById("inputEmail").value);
-}
-
