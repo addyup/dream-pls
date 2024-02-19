f356a7122545f9deb6de748b6e7badf5785c4846
adduup <adwait.uprety21@gmail.com>
2024-02-19 17:09:00 +0545

    tleaf

---
+++ diler/src/main/java/com/documentFiler/diler/DilerApplication.java
@@ -0,0 +1,13 @@
+package com.documentFiler.diler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DilerApplication.class, args);
+	}
+
+}
---
+++ diler/src/test/java/com/documentFiler/diler/DilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.documentFiler.diler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
